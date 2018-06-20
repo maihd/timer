2ceac3fb62bb151a5a81c630f21234deb78c84d0
MaiHD <maihd.dev@gmail.com>
2018-06-20 11:10:57 +0700

    Minimal update

--- timer.h
+++ timer.h
@@ -15,12 +15,12 @@ typedef struct
 {
     enum
     {
-	TIMER_STARTED,
-	TIMER_STOPPED,
+	    TIMER_STARTED,
+	    TIMER_STOPPED,
     }    state;
     long start;
     long ticks;
-} timer_t;    
+} timer_t;      
 
 __timer__ void   timer_init(timer_t* timer);
 __timer__ void   timer_start(timer_t* timer);
@@ -85,45 +85,38 @@ int perf_usleep(long us)
     
 int perf_nsleep(long ns)
 {
-    HANDLE        timer;
-    LARGE_INTEGER times;
-
-    /* Create timer for counting
-     */
-    timer = CreateWaitableTimer(NULL, TRUE, NULL);
-    if (!timer) return FALSE;
-
-    /* Settings timer with due time per 100ns units
-     */
-    times.QuadPart = -ns / 100;
-    if (!SetWaitableTimer(timer, &times, 0, NULL, NULL, FALSE))
+    /* Actually return NTSTATUS, and 'typedef LONG NTSTATUS;' =)) */
+    /* '#define NTAPI __stdcall' =)) */
+    typedef LONG (__stdcall * NtDelayExecutionFN)(BOOL, PLARGE_INTEGER);
+    static NtDelayExecutionFN NtDelayExecution;
+    
+    if (!NtDelayExecution)
     {
-	CloseHandle(timer);
-	return FALSE;
+        HMODULE module   = GetModuleHandle(TEXT("ntdll.dll"));
+        const char* func = "NtDelayExecution";
+        NtDelayExecution = (NtDelayExecutionFN)GetProcAddress(module, func);
+        if (!NtDelayExecution)
+        {
+            return FALSE;
+        }
     }
-
-    /* Wait until the timer end
-     */
-    WaitForSingleObject(timer, INFINITE);
-    CloseHandle(timer);
-
-    /* Progress done
-     */
+    
+    LARGE_INTEGER times;
+    times.QuadPart = -ns / 100;
+    NtDelayExecution(FALSE, &times);
     return TRUE;
 }
 
 long perf_counter(void)
 {
     LARGE_INTEGER result;
-    QueryPerformanceCounter(&result);
-    return result.QuadPart;
+    return QueryPerformanceCounter(&result) ? (long)result.QuadPart : 0;
 }
 
 long perf_frequency(void)
 {
     LARGE_INTEGER result;
-    QueryPerformanceFrequency(&result);
-    return result.QuadPart;
+    return QueryPerformanceFrequency(&result) ? (long)result.QuadPart : 0;
 }
 /* END OF WINDOWS */
 # elif __unix__
@@ -163,28 +156,28 @@ long perf_counter(void)
     if (has_monotonic())
     {
 #if HAVE_CLOCK_GETTIME
-	struct timespec now;
-	clock_gettime(CLOCK_ID, &now);
-	
-	/* Get counter in us */
-	ticks  = now.tv_sec;
-	ticks *= 1000 * 1000; /* To us */
-	ticks += now.tv_usec;
-	ticks += now.tv_msec * 1000;
+        struct timespec now;
+        clock_gettime(CLOCK_ID, &now);
+        
+        /* Get counter in us */
+        ticks  = now.tv_sec;
+        ticks *= 1000 * 1000; /* To us */
+        ticks += now.tv_usec;
+        ticks += now.tv_msec * 1000;
 #elif defined(__APPLE__)
-	ticks = mach_absolute_time();
+	    ticks = mach_absolute_time();
 #endif
     }
     else
     {
-	struct timeval now;
-	gettimeofday(&now, NULL);
-    
-	/* Get counter in us */
-	ticks  = now.tv_sec;
-	ticks *= 1000 * 1000; /* To us */
-	ticks += now.tv_usec;
-	ticks += now.tv_msec * 1000;
+        struct timeval now;
+        gettimeofday(&now, NULL);
+        
+        /* Get counter in us */
+        ticks  = now.tv_sec;
+        ticks *= 1000 * 1000; /* To us */
+        ticks += now.tv_usec;
+        ticks += now.tv_msec * 1000;
     }
     return ticks;
 }
@@ -194,18 +187,18 @@ long perf_frequency(void)
 #if HAVE_CLOCK_GETTIME
     if (has_monotonic())
     {
-	return 1000 * 1000 * 1000; /* ns per second */
+	    return 1000 * 1000 * 1000; /* ns per second */
     }
 #elif defined(__APPLE__)
     mach_timebase_info_data_t mach_info;
     kern_return_t ret = mach_time_base_info(&mach_info);
     if (ret == 0)
     {
-	long frequency;
-	frequency  = mach_info.denom;
-	frequency *= 1000 * 1000 * 1000;
-	frequency /= mach_info.numer;
-	return frequency;
+        long frequency;
+        frequency  = mach_info.denom;
+        frequency *= 1000 * 1000 * 1000;
+        frequency /= mach_info.numer;
+        return frequency;
     }
 #endif
     
